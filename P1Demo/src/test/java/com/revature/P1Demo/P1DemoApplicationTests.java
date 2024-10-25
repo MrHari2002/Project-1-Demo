4ab0a8203639f7493727d34cd8fa45b0593c95ce
benp3837 <petruzziellbj@alumni.vcu.edu>
2024-10-25 14:58:03 -0400

    RestTemplate test

--- P1Demo/src/test/java/com/revature/P1Demo/P1DemoApplicationTests.java
+++ P1Demo/src/test/java/com/revature/P1Demo/P1DemoApplicationTests.java
@@ -1,5 +1,7 @@
 package com.revature.P1Demo;
 
+import com.revature.models.DTOs.IncomingPetDTO;
+import com.revature.models.Pet;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -13,6 +15,29 @@ class P1DemoApplicationTests {
 	void contextLoads() {
 	}
 
+	//TestRestTemplate test for our PetController------------------------------------
 
+	//RestTemplate lets us send HTTP requests FROM OUR JAVA SERVER
+		//TestRestTemplate is a subclass that we can use for Testing
+
+	@Test
+	public void testInsertPet(){
+
+		//Instantiate a new TestRestTemplate object
+		TestRestTemplate restTemplate = new TestRestTemplate();
+
+		//Create a new IncomingPetDTO to use in our test
+		IncomingPetDTO petDTO = new IncomingPetDTO("Dog", "Fido", 1);
+
+		//Send a POST request to /pets and save the returned Pet to run our tests on
+		                    //{verb}ForObject  //URL, Request Body, Response Type
+		Pet p = restTemplate.postForObject("http://localhost:7777/pets", petDTO, Pet.class);
+
+		//basic tests, make sure our Pet object came back as expected
+		assertNotNull(p);
+		assertNotEquals(p.getPetId(), 0);
+		assertEquals(p.getSpecies(), "Dog");
+
+	}
 
 }
