b7947a05780304e1dfe65a551a6b4b12b6ecfed4
benp3837 <petruzziellbj@alumni.vcu.edu>
2024-11-06 13:53:41 -0500

    GET request

---
+++ P1DemoFrontEnd/p1-frontend/src/Components/Pet/PetContainer.tsx
@@ -0,0 +1,41 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { Container } from "react-bootstrap"
+
+export const PetContainer:React.FC = () => {
+
+    //we'll store a state object that contains an Array of PetInterfaces
+    //TODO: make the pet interface, but for now, we can just use an any[]
+    const[pets, setPets] = useState([])
+
+    //Defining a useEffect that calls the function that gets pets by userId
+    useEffect(()=>{
+        getPetsByUserId()
+    }, []) //this useEffect triggers on component load
+
+
+    //The function that gets all pets with an axios GET request
+    const getPetsByUserId = async () => {
+
+        //axios GET request 
+        //TODO: hardcoding user ID for now, but this will be the id of the logged in user
+        const response = await axios.get("http://localhost:7777/pets/user/1")
+        //TODO: then(), catch() etc
+
+        //populate the pets state object
+        setPets(response.data) //data holds the data send in the response body
+
+        console.log(response.data)
+    }
+
+
+    return(
+        /* TODO: navbar thing? for navigation options etc */
+        <Container>
+
+              
+
+        </Container>
+    )
+
+}
